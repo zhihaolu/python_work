3196e60173522007886c34fb700705bc19b67ab0
zhihao lu <2459555737@qq.com>
2021-06-18 00:21:44 +0800

    SnipTool

---
+++ C_sharp/SnipTool/SnipTool/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace SnipTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.截图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.截图ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(241, 133);
+            this.contextMenuStrip1.Text = "开始";
+            // 
+            // 截图ToolStripMenuItem
+            // 
+            this.截图ToolStripMenuItem.Name = "截图ToolStripMenuItem";
+            this.截图ToolStripMenuItem.Size = new System.Drawing.Size(116, 30);
+            this.截图ToolStripMenuItem.Text = "截图";
+            this.截图ToolStripMenuItem.Click += new System.EventHandler(this.截图ToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(113, 6);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(116, 30);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "SnipTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 截图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ C_sharp/SnipTool/SnipTool/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnipTool
+{
+    public partial class Form1 : Form
+    {
+        int x, y;
+        Graphics gFrom;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 截图ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            contextMenuStrip1.Show(Cursor.Position);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                int startX = x, startY = y, endX = e.X, endY = e.Y, temp;
+                if(startX > endX)
+                {
+                    temp = startX;
+                    startX = endX;
+                    endX = temp;
+                }
+                if(startY > endY)
+                {
+                    temp = startY;
+                    startY = endY;
+                    endY = temp;
+                }
+                /*
+                Graphics g = this.CreateGraphics();
+                g.Clear(this.BackColor);
+                if(startX != endX && startY != endY)
+                {
+                    g.DrawRectangle(Pens.Red, startX, startY, endX - startX, endY - startY);
+                }
+                */
+                
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                int startX = x, startY = y, endX = e.X, endY = e.Y, temp;
+                if(startX == endX || startY == endY)
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                    return;
+                }
+                if (startX > endX)
+                {
+                    temp = startX;
+                    startX = endX;
+                    endX = temp;
+                }
+                if (startY > endY)
+                {
+                    temp = startY;
+                    startY = endY;
+                    endY = temp;
+                }
+                Size s = new Size(endX - startX, endY - startY);
+                this.WindowState = FormWindowState.Minimized;
+                using(Bitmap bmp = new Bitmap(s.Width, s.Height))
+                {
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.CopyFromScreen(startX, startY, 0, 0, s);
+                    if (保存ToolStripMenuItem.Checked)
+                    {
+                        string filename = string.Format(@"d:\截图{0}.png", DateTime.Now.ToString("yyyyMMddHHmmss"));
+                        bmp.Save(filename, System.Drawing.Imaging.ImageFormat.Png);
+                        System.Diagnostics.Process.Start(filename);
+                    }
+                  
+                    Clipboard.SetImage(bmp);   // 复制到剪贴板
+                }
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ( e.KeyCode == Keys.Escape)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            保存ToolStripMenuItem.Checked = !保存ToolStripMenuItem.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "";
+            this.ControlBox = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Opacity = 0.5;
+            this.WindowState = FormWindowState.Maximized;
+            gFrom = this.CreateGraphics();
+        }
+    }
+}
