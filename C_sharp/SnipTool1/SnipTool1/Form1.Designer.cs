50fb54d626ae25c9d9a85ad2a33fc71fd2ebaacf
zhihao lu <2459555737@qq.com>
2021-06-18 00:23:45 +0800

    Csharp wordSnipTool to gitee

---
+++ C_sharp/SnipTool1/SnipTool1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace SnipTool1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.snipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.snipToolStripMenuItem,
+            this.autoSaveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(167, 100);
+            // 
+            // snipToolStripMenuItem
+            // 
+            this.snipToolStripMenuItem.Name = "snipToolStripMenuItem";
+            this.snipToolStripMenuItem.Size = new System.Drawing.Size(166, 30);
+            this.snipToolStripMenuItem.Text = "Snip";
+            this.snipToolStripMenuItem.Click += new System.EventHandler(this.snipToolStripMenuItem_Click);
+            // 
+            // autoSaveToolStripMenuItem
+            // 
+            this.autoSaveToolStripMenuItem.Name = "autoSaveToolStripMenuItem";
+            this.autoSaveToolStripMenuItem.Size = new System.Drawing.Size(166, 30);
+            this.autoSaveToolStripMenuItem.Text = "Auto Save";
+            this.autoSaveToolStripMenuItem.Click += new System.EventHandler(this.autoSaveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(163, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(166, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "SnipTool";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SnipTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem snipToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoSaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ C_sharp/SnipTool1/SnipTool1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnipTool1
+{
+    public partial class Form1 : Form
+    {
+        int x, y;
+        Graphics gForm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void snipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 开始新的截图
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 退出小工具
+            Application.Exit();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            contextMenuStrip1.Show(Cursor.Position);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int startX = x, startY = y, endX = e.X, endY = e.Y, temp;
+                if(startX > endX)
+                {
+                    temp = startX;
+                    startX = endX;
+                    endX = temp;
+                }
+                if(startY > endY)
+                {
+                    temp = startY;
+                    startY = endY;
+                    endY = temp;
+                }
+                Graphics g = this.CreateGraphics();
+                g.Clear(this.BackColor);
+                if (startX != endX && startY != endY)
+                {
+                    // 实时显示截取的屏幕图像让用户预览截图内容
+                    gForm.CopyFromScreen(startX, startY, startX, startY, new Size(endX - startX, endY - startY));
+                    // 画红框，让用户看清截图区域
+                    g.DrawRectangle(Pens.Red, startX, startY, endX - startX, endY - startY);
+                }
+            }
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                int startX = x, startY = y, endX = e.X, endY = e.Y, temp;
+                if(startX == endX || startY == endY)
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                    return;
+                }
+                if (startX > endX)
+                {
+                    temp = startX;
+                    startX = endX;
+                    endX = temp;
+                }
+                if (startY > endY)
+                {
+                    temp = startY;
+                    startY = endY;
+                    endY = temp;
+                }
+                Size s = new Size(endX - startX, endY - startY);
+                this.WindowState = FormWindowState.Minimized;
+                using(Bitmap bmp = new Bitmap(s.Width, s.Height))
+                {
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.CopyFromScreen(startX, startY, 0, 0, s);
+                    if (autoSaveToolStripMenuItem.Checked)
+                    {
+                        string filename = string.Format(@"d:\截图{0}.png", DateTime.Now.ToString("yyyyMMddHHmmss"));
+                        bmp.Save(filename, System.Drawing.Imaging.ImageFormat.Png);
+                        System.Diagnostics.Process.Start(filename);
+                    }
+                    Clipboard.SetImage(bmp);
+                    
+                }
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void autoSaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            autoSaveToolStripMenuItem.Checked = !autoSaveToolStripMenuItem.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 窗体初始化
+            this.Text = "";
+            this.ControlBox = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Opacity = 0.5;
+            this.WindowState = FormWindowState.Maximized;
+            gForm = this.CreateGraphics();
+        }
+    }
+}
